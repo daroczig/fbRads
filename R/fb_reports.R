da8f36625015a514fc39c310f906d6288e0cb5f5
Gergely Daroczi <gergely.daroczi@card.com>
2015-08-11 14:35:35 -0700

    quick and dirty wrapper around the ad stats API endpoint

--- R/fb_reports.R
+++ R/fb_reports.R
@@ -50,3 +50,35 @@ fb_reportstats_ad <- function(fbacc, ...) {
     do.call(rbind, l)
 
 }
+
+
+#' Get Ad Stats
+#' @inheritParams fbad_request
+#' @param ids adgroup ids
+#' @references \url{https://developers.facebook.com/docs/marketing-api/adstatistics/v2.3#read}
+#' @note This API endpoint is be soon deprecated and only available with v2.3. Use the new Insight feature, if possible.
+#' @return list (raw parsed JSON) to be post-processed
+#' @examples \dontrun{
+#' res <- fb_stats_ad(...)
+#' library(rlist)
+#' list.stack(list.select(res, adgroup_id, impressions, clicks, spent, unique_impressions))
+#' }
+#' @export
+fb_stats_ad <- function(fbacc, ids) {
+
+    fbacc <- fbad_check_fbacc()
+
+    if (fbacc$api_version > '2.3') {
+        stop('Old statistics APIs are being deprecated in favor of Insights Edge introduced with v2.4 of the Facebook Marketing API.')
+    }
+
+    ## get report
+    res <- fbad_request(fbacc,
+        path   = 'stats',
+        method = 'GET',
+        params = list(ids = paste(ids, collapse = ',')))
+
+    ## parse JSON
+    fromJSON(res, flatten = TRUE)
+
+}
