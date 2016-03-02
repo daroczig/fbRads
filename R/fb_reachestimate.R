cc15a2066ddab347d9ce9c740736ee05db712f96
Gergely Daroczi <gergely.daroczi@card.com>
2016-03-02 14:11:33 -0800

    get reachestimate work with v2.5

--- R/fb_reachestimate.R
+++ R/fb_reachestimate.R
@@ -1,29 +1,47 @@
 #' Query for reach estimate for given targeting spec
 #' @inheritParams fbad_request
-#' @param targeting_spec lists of targeting spec characteristics
+#' @param targeting_spec lists of targeting spec characteristics as described at \url{https://developers.facebook.com/docs/marketing-api/targeting-specs/v2.5}
 #' @param currency string
+#' @param optimize_for what are you optimizing for in the planned Ad Set?
 #' @return list
 #' @export
-#' @references \url{https://developers.facebook.com/docs/marketing-api/reachestimate/v2.4}
+#' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-account/reachestimate/}
 #' @examples \dontrun{
 #' targetspec <- list(
 #'   age_min = unbox(24),
 #'   age_max = unbox(55),
 #'   geo_locations = list(countries = 'US'))
+#' fbad_reachestimate(targeting_spec = targetspec)
 #' }
-fbad_reachestimate <- function(fbacc, targeting_spec, currency = 'USD') {
+fbad_reachestimate <- function(fbacc, targeting_spec, currency = 'USD',
+                               optimize_for = c('NONE', 'APP_INSTALLS', 'CLICKS',
+                                                'ENGAGED_USERS', 'EXTERNAL',
+                                                'EVENT_RESPONSES', 'IMPRESSIONS',
+                                                'LINK_CLICKS', 'OFFER_CLAIMS',
+                                                'OFFSITE_CONVERSIONS', 'PAGE_ENGAGEMENT',
+                                                'PAGE_LIKES', 'POST_ENGAGEMENT',
+                                                'REACH', 'SOCIAL_IMPRESSIONS',
+                                                'VIDEO_VIEWS')) {
 
     fbacc <- fbad_check_fbacc()
     if (missing(targeting_spec) | !is.list(targeting_spec))
         stop('An R list targetspec is required.')
 
+    ## default parameters
+    params = list(
+        currency       = currency,
+        targeting_spec = toJSON(targeting_spec))
+
+    ## optimize_for was introduced in v2.5
+    if (fb_api_version() >= '2.5') {
+        params$optimize_for <- match.arg(optimize_for)
+    }
+
     ## get results
     res <- fbad_request(fbacc,
         path   = paste0('act_', fbacc$account_id, '/reachestimate'),
         method = "GET",
-        params = list(
-            currency       = currency,
-            targeting_spec = toJSON(targeting_spec)))
+        params = params)
 
     ## parse JSON
     res <- fromJSON(res)
@@ -31,6 +49,11 @@ fbad_reachestimate <- function(fbacc, targeting_spec, currency = 'USD') {
     ## error handling
     if (isTRUE(res$unsupported) | isTRUE(res$bid_estimations$unsupported) | isTRUE(res$data$bid_estimations$unsupported))  {
         stop('Invalid parameters provided, check your targeting_spec.')
+    }
+
+    ## v2.5 returns a "data" list
+    if (fb_api_version() >= '2.5') {
+        res <- res$data
     }
 
     ## return
