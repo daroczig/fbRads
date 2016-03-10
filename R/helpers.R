ba87175f12da3480623991405705483cd57de4f7
Gergely Daroczi <gergely.daroczi@card.com>
2016-03-10 14:37:54 -0800

    more general fix for #11 when handling fn name references

--- R/helpers.R
+++ R/helpers.R
@@ -7,5 +7,12 @@ this_function_name <- function() {
            ## we have found the function name
            deparse(sc),
            ## this is not the actual function name but a call referring to that
-           deparse(tail(sc, 1)[[1]]))
+    ifelse(sc[[1]] == '::',
+           ## like a double colon
+           deparse(tail(sc, 1)[[1]]),
+           ## the calling function is returned by "get" or similar
+           stop(paste("Something is off with the calling function's name:",
+                      deparse(sc), "-- expecting a single function name here.",
+                      'If in doubt, report this issue on <<bug.report()>>.'))
+           ))
 }
