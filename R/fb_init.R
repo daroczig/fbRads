afa6d4a1243042f0449d670e74e7fea32857d029
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-07-21 10:50:26 +0200

    retry on "Connection timed out"

--- R/fb_init.R
+++ R/fb_init.R
@@ -157,7 +157,8 @@ fbad_request <- function(fbacc, path, method = c('GET', 'POST', 'DELETE'), param
         ## temporary network issue?
         if (grepl('Network is unreachable', curlres$message) |
             grepl('Empty reply from server', curlres$message) |
-            grepl('Unknown SSL protocol error', curlres$message)) {
+            grepl('Unknown SSL protocol error', curlres$message) |
+            grepl('Failed to connect to graph.facebook.com', curlres$message)) {
 
             ## log it
             flog.error(paste('Possible network error:', curlres$message))
