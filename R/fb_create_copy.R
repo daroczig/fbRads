192a2eaec03bc4297b0fa87e8b1bb21023a936c8
ArbenKqiku <arben.kqiku@gmail.com>
2020-10-12 21:31:54 +0200

    Added possibility to define and end_time

--- R/fb_create_copy.R
+++ R/fb_create_copy.R
@@ -1,11 +1,12 @@
 #' Copy Ad Campaign or Ad Set
 #' @inheritParams fbad_request
-#' @param id id of the campaign or adset you want to create a copy of
+#' @param id (character) id of the campaign or adset you want to create a copy of
+#' @param end_time (datetime) The end time of the ad set
 #' @param ... further arguments passed to the API endpoint
 #' @export
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/copies/}
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/copies/}
-fbad_create_copy <- function(fbacc, id, ...) {
+fbad_create_copy <- function(fbacc, id, end_time, ...) {
     
     fbacc <- fbad_check_fbacc()
     
@@ -14,7 +15,8 @@ fbad_create_copy <- function(fbacc, id, ...) {
     
     ## build params list
     params <- list(
-        id = id)
+        id = id,
+        end_time = end_time)
     
     ## drop NULL args
     params <- as.list(unlist(params, recursive = FALSE))
@@ -26,3 +28,23 @@ fbad_create_copy <- function(fbacc, id, ...) {
                  params = params)
     
 }
+
+token = 'EAADhONh9vckBAFUT4VE468jfQKg7g9YGKgF25ZBASkhzdAU9H6BhNq1Dea5xZAcQrzVIv8kwzZCvpfu2Y9SE7G6psPUKfQ0LtCUZAtWUeSofzuOpeAXKRyRuDAnlS35WW8AeZB8LKZBorZADZAxUwXjBb9OBKleiWj32ZBDVOz7n2qzXZAsZBZA1rJbi3w7PR19JGK8ZD'
+# Define account id - Facebook API > Tests
+account_id = "347374566526931"
+
+library(fbRads)
+library(jsonlite)
+library(data.table)
+library(RCurl)
+library(lubridate)
+
+# 3 Initialization ----
+# Initialize specific account
+fbacc = fbad_init(accountid = account_id, token = token, version = '8.0')
+
+end_time = as_datetime("2020-10-25 23:59:59 PDT") 
+
+fbad_create_copy(fbacc, id = "23845900069090648", end_time = end_time)
+
+?fbad_init
