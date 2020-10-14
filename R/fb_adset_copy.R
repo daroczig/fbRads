2c8212a7f8fa44f1431f7f032006167462173366
ArbenKqiku <arben.kqiku@gmail.com>
2020-10-14 20:50:05 +0200

    Removed periods at the end of the description of parameters

--- R/fb_adset_copy.R
+++ R/fb_adset_copy.R
@@ -1,14 +1,14 @@
 #' Copy Ad Campaign or Ad Set
 #' @inheritParams fbad_request
 #' @param adset_id (numeric string or integer) id adset you want to create a copy of
-#' @param campaign_id (numeric string or integer) Single ID of a campaign to make parent of the copy. Ignore if you want to keep the copy under the original campaign parent.
-#' @param start_time (datetime) The start time of the ad set. If not set, the copied adset will inherit the start time from the original set.
+#' @param campaign_id (numeric string or integer) Single ID of a campaign to make parent of the copy. Ignore if you want to keep the copy under the original campaign parent
+#' @param start_time (datetime) The start time of the ad set. If not set, the copied adset will inherit the start time from the original set
 #' @param end_time (datetime) The end time of the ad set
-#' @param deep_copy (boolean) Default value: false. Whether to copy all the child ads.
+#' @param deep_copy (boolean) Default value: false. Whether to copy all the child ads
 #' @param status_option (string) enum {ACTIVE, PAUSED, INHERITED_FROM_SOURCE}
 #' @param rename_strategy (string) enum {DEEP_RENAME, ONLY_TOP_LEVEL_RENAME, NO_RENAME}
-#' @param rename_prefix (string) a prefix to copy names. Defaults to null if not provided.
-#' @param rename_suffix (string) a suffix to copy names. Defaults to null if not provided.
+#' @param rename_prefix (string) a prefix to copy names. Defaults to null if not provided
+#' @param rename_suffix (string) a suffix to copy names. Defaults to null if not provided
 #' @param ... further arguments passed to the API endpoint
 #' @export
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/copies/}
