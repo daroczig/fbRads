5794a06db519c3c8c7d0cfea8785f41d1d1dd343
ArbenKqiku <arben.kqiku@gmail.com>
2020-10-14 20:27:13 +0200

    added unit tests + the possibility to select a campaign id when you copy an ad set

--- R/fb_copy_adset.R
+++ R/fb_copy_adset.R
@@ -1,6 +1,7 @@
 #' Copy Ad Campaign or Ad Set
 #' @inheritParams fbad_request
-#' @param id (character) id of the campaign or adset you want to create a copy of
+#' @param adset_id (numeric string or integer) id adset you want to create a copy of
+#' @param campaign_id (numeric string or integer) Single ID of a campaign to make parent of the copy. Ignore if you want to keep the copy under the original campaign parent.
 #' @param start_time (datetime) The start time of the ad set. If not set, the copied adset will inherit the start time from the original set.
 #' @param end_time (datetime) The end time of the ad set
 #' @param deep_copy (boolean) Default value: false. Whether to copy all the child ads.
@@ -12,8 +13,9 @@
 #' @export
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/copies/}
 #' @references \url{https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/copies/}
-fbad_copy_adset <- function(fbacc, 
+fbad_copy_adset <- function(fbacc,
                              adset_id,
+                             campaign_id = NULL,
                              start_time = NULL,
                              end_time = NULL, 
                              deep_copy = NULL,
@@ -37,21 +39,25 @@ fbad_copy_adset <- function(fbacc,
     }
     
     # rename options
-    if(!is.null(rename_strategy) && !is.null(rename_prefix)){
+    if(is.null(rename_strategy)){
+        rename_options = NULL
+    }else if((rename_strategy == "DEEP_RENAME" | rename_strategy == "ONLY_TOP_LEVEL_RENAME") & is.null(rename_prefix) & is.null(rename_suffix)){
+        stop("You have selected 'DEEP_RENAME' or 'ONLY_TOP_LEVEL_RENAME' as the argument rename_strategy. You need to specify either the rename_prefix argument, the rename_suffix argument or both")
+    }else if(rename_strategy == "NO_RENAME" & !is.null(rename_prefix) || !is.null(rename_suffix)){
+        stop("Your rename_stratey is 'NO_RENAME', therefore, you should not select argument rename_prefix or rename_suffix")
+    }else if(!is.null(rename_strategy) & !is.null(rename_prefix)){
         rename_options = list(rename_strategy = rename_strategy,
                               rename_prefix = rename_prefix)
-    }else if(!is.null(rename_strategy) && !is.null(rename_suffix)){
+    }else if(!is.null(rename_strategy) & !is.null(rename_suffix)){
         rename_options = list(rename_strategy = rename_strategy,
                               rename_suffix = rename_suffix)
-    }else if(is.null(rename_prefix) && is.null(rename_suffix) && !is.null(rename_suffix)){
+    }else if(is.null(rename_prefix) & is.null(rename_suffix) & !is.null(rename_suffix)){
         rename_options = list(rename_strategy = rename_strategy)
-    }else{
-        rename_options = NULL
     }
     
     ## build params list
     params <- list(
-        adset_id = adset_id,
+        campaign_id = campaign_id,
         start_time = start_time,
         end_time = end_time,
         deep_copy = deep_copy,
@@ -64,11 +70,13 @@ fbad_copy_adset <- function(fbacc,
     ## drop NULL args
     params <- as.list(unlist(params, recursive = FALSE))
     
-    ## post request to copy adset
-    fbad_request(fbacc,
+    # post request to copy adset
+    final_request = fbad_request(fbacc,
                  path   = paste0(adset_id, "/copies?access_token=", fbacc$access_token),
                  method = "POST",
                  params = params)
+    
+    return(substr(final_request, 1, 18))
     
 }
 
@@ -89,13 +97,15 @@ fbacc = fbad_init(accountid = account_id, token = token, version = '8.0')
 start_time = as_datetime("2020-10-20 23:59:59 PDT")
 end_time = as_datetime("2020-10-27 10:10:10 UTC")
 
-fbad_create_copy(fbacc, adset_id = "23845893193900648",
+campaign_id = "23845914189980648"
+
+fbad_copy_adset(fbacc, 
+                 adset_id = "23845893193900648",
+                 campaign_id = campaign_id,
                  deep_copy = TRUE,
                  status_option = "ACTIVE",
                  rename_strategy = "DEEP_RENAME",
                  rename_prefix = "Morning-2")
 
 # Install the released version from CRAN
-install.packages("testthat")
 library(testthat)
-
---
+++ tests/test_fb_copy_adset.R
@@ -0,0 +1,242 @@
+source("Clients/Data Science/fbRads-Arben/R/fb_copy_adset.R")
+
+# 1 Remove one argument at the time and see if the function still works. The only compulsory argument is adset_id ----
+expected = "{\"copied_adset_id\""
+
+# 1.1
+expect_equal(fbad_copy_adset(fbacc, 
+                              adset_id = "23845893193900648",
+                              deep_copy = TRUE,
+                              start_time = start_time,
+                              end_time = end_time,
+                              status_option = "ACTIVE",
+                              rename_strategy = "DEEP_RENAME",
+                              rename_prefix = "Morning-2",
+                              rename_suffix = "Hello"),
+             
+             expected)
+
+# 1.2
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "Morning-2"),
+             
+             expected)
+
+# 1.3
+error = "You have selected 'DEEP_RENAME' as the argument rename_strategy. You need to specify either the rename_prefix argument, the rename_suffix argument or both"
+
+expect_error(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME"),
+             
+                   error)
+
+# 1.4
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE,
+                             start_time = "start_time",
+                             end_time = end_time,
+                             status_option = "ACTIVE"),
+             
+             expected)
+
+# 1.5
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time),
+             
+             expected)
+
+# 1.6
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE,
+                             start_time = start_time),
+             
+             expected)
+
+# 1.7
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648",
+                             deep_copy = TRUE),
+             
+             expected)
+
+# 1.8
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = "23845893193900648"),
+             
+             expected)
+
+# 2 Test wrong argument types and see what errors are thrown ----
+
+# 2.1 deep copy
+error = "Some critical FB query error here."
+
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = "TRUEE",
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+# 2.2 start_time
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = "start_time",
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+# 2.3 start_time
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = "end_time",
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+# 2.4 status_option
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "TRUE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+
+# 2.4 rename_strategy
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "TRUE",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+# 2.4 rename_strategy
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "TRUE",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "NO_RENAME",
+                             rename_prefix = "Morning-2",
+                             rename_suffix = "Hello"),
+             
+             error)
+
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "ONLY_TOP_LEVEL_RENAME"),
+             
+             error)
+
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME"),
+             
+             error)
+
+expect_equal(fbad_copy_adset(fbacc, 
+                             adset_id = 23845893193900648,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "testthat"),
+             
+             error)
+
+# 3 Test if adset_id is missing ----
+error = "Argument missing. An adset id is required."
+
+# 3.1 missing ad set
+expect_error(fbad_copy_adset(fbacc, 
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "testthat"),
+             
+             error)
+
+# 3.2 not an ad set id
+error = "This adset id does not exists. Please provide a valid adset id."
+expect_error(fbad_copy_adset(fbacc,
+                             adset_id = 34234234,
+                             deep_copy = TRUE,
+                             start_time = start_time,
+                             end_time = end_time,
+                             status_option = "ACTIVE",
+                             rename_strategy = "DEEP_RENAME",
+                             rename_prefix = "testthat"),
+             
+             error)
+
+
+
+
+
+
+
