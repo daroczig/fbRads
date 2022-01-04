ca7a31b7825e2b472f8003760ec6449cddc31966
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2022-01-04 16:18:20 +0100

    bump default API version to v12.0

--- R/fb_init.R
+++ R/fb_init.R
@@ -1,7 +1,7 @@
 #' Returns the most recent version of the supported Facebook Marketing API
 #' @return string
 #' @export
-fb_api_most_recent_version <- function() '10.0'
+fb_api_most_recent_version <- function() '12.0'
 
 #' Returns the currently used version of the Facebook Marketing API
 #' @return string
