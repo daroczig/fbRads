5b7e239670cdd4197c92879ac0b4e1c8ffa3db8c
Attila Nagy <nagy.attila@gmail.com>
2023-07-25 14:48:39 +0200

    Switch to v17

--- R/fb_init.R
+++ R/fb_init.R
@@ -1,7 +1,7 @@
 #' Returns the most recent version of the supported Facebook Marketing API
 #' @return string
 #' @export
-fb_api_most_recent_version <- function() '15.0'
+fb_api_most_recent_version <- function() '17.0'
 
 #' Returns the currently used version of the Facebook Marketing API
 #' @return string
